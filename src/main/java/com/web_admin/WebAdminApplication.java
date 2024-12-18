ab8302059aa09b7b39b447fb2f1cc8a1bf28898c
lilyindr <lily@ppu.co.id>
2024-12-18 11:17:59 +0700

    first time

---
+++ src/main/java/com/web_admin/WebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.web_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAdminApplication.class, args);
+	}
+
+}
